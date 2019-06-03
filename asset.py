c5c17109dd436990526e60c2038780e2493a019a
LorenzoBellone <44533233+LorenzoBellone@users.noreply.github.com>
2019-06-03 18:22:15 +0200

    Update asset.py

--- asset.py
+++ asset.py
@@ -1,3 +1,5 @@
+
+
 def nearest_servers(id):
     nations = ["china", "usa", "india", "japan", "brazil"]
     index = nations.index(id)
@@ -9,9 +11,35 @@ def nearest_servers(id):
         distances[index][distances[index].index(min(distances[index]))] += 10
     return list_nations
 
+
 def RTT(server, client):
     nations = ["china", "usa", "india", "japan", "brazil"]
-    distances = [[0, 4, 2, 1, 3], [4, 0, 5, 3, 2], [1, 5, 0, 2, 6], [1, 3, 2, 0, 4], [3, 2, 4, 6, 0]]
+    distances = [[0, 11004, 3783, 2100, 16950],
+                 [1104, 0, 11770, 10864, 6835],
+                 [3783, 11770, 0, 5848, 14258],
+                 [2100, 10864, 5848, 0, 17692],
+                 [16950, 6835, 14258, 17962, 0]]
     ind_server = nations.index(server)
     ind_client = nations.index(client)
     return distances[ind_server][ind_client]
+
+
+def global_service_times(server, dict, current_time, current_capacity):
+    for i in (dict[server]["current_requests"].keys()):
+        # The new packet dimension, equal to the previous dimension minus the amount of bits downloaded in the meantime
+        dict[server]["current_requests"][i][2] = dict[server]["current_requests"][i][2] - dict[server]["current_requests"][i][1]*(current_time - dict[server]["current_requests"][i][3])
+        # The new current time, referred to the last update of shared capacity
+        dict[server]["current_requests"][i][3] = current_time
+        # The new shared capacity for the packet
+        dict[server]["current_requests"][i][1] = current_capacity
+        # THe new service time, equal to the current dimension of the packet over the current shared capacity
+        dict[server]["current_requests"][i][0] = dict[server]["current_requests"][i][2]/dict[server]["current_requests"][i][1]
+        print("New service time for id", i, " : ", dict[server]["current_requests"][i][0])
+    return dict
+
+
+def evaluate_cost(last_update, time, server):
+    costs = {"china": 0.104, "usa": 0.0976, "india": 0.0896, "japan" : 0.1088, "brazil" : 0.1344}
+    return (time - last_update)*costs[server]
+
+
