86505e5ea4d392c75b3a92c35fc02a0d3eedc194
LorenzoBellone <44533233+LorenzoBellone@users.noreply.github.com>
2019-06-13 16:55:53 +0200

    Update asset.py

--- asset.py
+++ asset.py
@@ -4,12 +4,16 @@ import random
 def nearest_servers(id):
     nations = ["china", "usa", "india", "japan", "brazil"]
     index = nations.index(id)
-    distances = [[0, 4, 2, 1, 3],  [4, 0, 5, 3, 2], [1, 5, 0, 2, 6], [1, 3, 2, 0, 4], [3, 2, 4, 6, 0]]
+    distances = [[0, 11004, 3783, 2100, 16950],
+                 [1104, 0, 11770, 10864, 6835],
+                 [3783, 11770, 0, 5848, 14258],
+                 [2100, 10864, 5848, 0, 17692],
+                 [16950, 6835, 14258, 17962, 0]]
     list_nations = []
     for i in range(len(nations)):
         closest = nations[distances[index].index(min(distances[index]))]
         list_nations.append(closest)
-        distances[index][distances[index].index(min(distances[index]))] += 10
+        distances[index][distances[index].index(min(distances[index]))] += max(distances[index])+1
     return list_nations
 
 
@@ -35,19 +39,21 @@ def global_service_times(server, dict, name_request, current_time, current_capac
     dict[server]["current_requests"][name_request][1] = current_capacity
     # THe new service time, equal to the current dimension of the packet over the current shared capacity
     dict[server]["current_requests"][name_request][0] = dict[server]["current_requests"][name_request][2]/dict[server]["current_requests"][name_request][1]
-    print("New service time for id", name_request, " : ", round(dict[server]["current_requests"][name_request][0], 5))
+    #print("New service time for id", name_request, " : ", round(dict[server]["current_requests"][name_request][0], 10))
     return dict
 
 
 def evaluate_cost(last_update, time, server):
-    costs = {"china": 0.104, "usa": 0.0976, "india": 0.0896, "japan" : 0.1088, "brazil" : 0.1344}
+    costs = {"china": 0.104/60, "usa": 0.0976/60, "india": 0.0896/60, "japan" : 0.1088/60, "brazil" : 0.1344/60}
     return (time - last_update)*costs[server]
 
 
 def arrival_function(time, nation, arrival_rate):
     nation_timezone = {"china": 8, "usa": -5, "india": 5, "brazil": -3, "japan": 9}
-    if (time % 24) < (8 + nation_timezone[nation]) or (time % 24 > (20 + nation_timezone[nation])):
+    hour_to_sec = 60*60
+    if (time % 24*hour_to_sec) < (8 + nation_timezone[nation])*hour_to_sec or (time % 24*hour_to_sec > (20 + nation_timezone[nation])*hour_to_sec):
         arrival_rate2 = copy.deepcopy(arrival_rate * random.uniform(0.05, 0.15))
     else:
         arrival_rate2 = copy.deepcopy(arrival_rate * random.uniform(0.8, 1.2))
     return arrival_rate2
+
